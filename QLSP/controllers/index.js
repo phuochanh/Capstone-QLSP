60399e39b54e9c0333bf3de9f4e8969ffdeeb4e0
Phuoc Hanh <hanhcp2013@gmail.com>
2022-12-02 19:32:22 +0700

    done

--- QLSP/controllers/index.js
+++ QLSP/controllers/index.js
@@ -1,20 +1,19 @@
 var productService = new ProductService();
 var allItem = [];
-function getProductList(){
-    productService.getList().then(function(response){
-        renderProductList(response.data);
-        allItem = response.data;
-    });   
+function getProductList() {
+  productService.getList().then(function (response) {
+    renderProductList(response.data);
+    allItem = response.data;
+  });
 }
-function domId(id){
-    return document.getElementById (id);
+function domId(id) {
+  return document.getElementById(id);
 }
-function renderProductList(data){
-    var content = "";
+function renderProductList(data) {
+  var content = "";
 
   for (var i = 0; i < data.length; i++) {
     content += `
-    
                   <div class="card" style="color:white">
                   <div class="img-container">
                     <img id="image" class="product-img" src=${data[i].img} alt="">
@@ -49,78 +48,59 @@ function renderProductList(data){
   domId("main-cart").innerHTML = content;
 }
 
-
 // 4. Chọn sản phẩm
-domId("chooseProduct").onchange = function (event){
-    var typeProduct =  event.target.value; // Lấy giá trị của ô option
-    
-    productService.getList().then(function(response){
-        renderProductList(filterProduct(response.data,typeProduct));
-});
-}
+domId("chooseProduct").onchange = function (event) {
+  var typeProduct = event.target.value; // Lấy giá trị của ô option
 
-function filterProduct(data,type){
-    var result = [];
-    if (type === "Chọn Sản Phẩm") return data;
-    for(var i = 0; i < data.length; i++){
-        if(data[i].type.toLowerCase() === type.toLowerCase()){
-            result.push(data[i]);
-        }
-    }  
-    return result;
+  productService.getList().then(function (response) {
+    renderProductList(filterProduct(response.data, typeProduct));
+  });
+};
+
+function filterProduct(data, type) {
+  var result = [];
+  if (type === "Chọn Sản Phẩm") return data;
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].type.toLowerCase() === type.toLowerCase()) {
+      result.push(data[i]);
+    }
+  }
+  return result;
 }
 
 //5. Thêm sản phẩm vào giỏ hàng
-var cart =  [];
-function addProduct(i){
-
-  // let storage = localStorage.getItem("cart");
-  // if(storage){
-  //   cart = JSON.parse(storage)
-  // }
-
-  if(checkCart(allItem[i].id) >= 0){
+var cart = [];
+function addProduct(i) {
+  if (checkCart(allItem[i].id) >= 0) {
     updateQuantity(checkCart(allItem[i].id));
-  }else{
-    let cartItem = {
-      product:{
-        id: 0,    
-        img: "",     
-        name:"",
+  } else {
+    var cartItem = {
+      product: {
+        id: 0,
+        img: "",
+        name: "",
         sl: 0,
-        price: 0, 
+        price: 0,
         money: 0,
-      }, 
-      quantity:1,
-    };  
-
-cartItem.product.id = allItem[i].id;
-cartItem.product.img = allItem[i].img;
-cartItem.product.price = allItem[i].price;
-cartItem.product.name = allItem[i].name;
-cartItem.product.sl = 1;
-cartItem.product.money = cartItem.product.price * cartItem.product.sl;
-
+      },
+      quantity: 1,
+    };
+    cartItem.product.id = allItem[i].id;
+    cartItem.product.img = allItem[i].img;
+    cartItem.product.price = allItem[i].price;
+    cartItem.product.name = allItem[i].name;
+    cartItem.product.sl = 1;
+    cartItem.product.money = cartItem.product.price * cartItem.product.sl;
     cart.push(cartItem);
   }
-  
-  // localStorage.setItem("cart", JSON.stringify(cart));
-var quantity = 0;
-for(var i = 0; i < cart.length; i++){
-  quantity += cart[i].quantity;
-}
-domId("total-qty").innerHTML = quantity;
-
-console.log(cart);
-renderCart(cart);
-saveData();
-
+  renderCart(cart);
+  saveData();
 }
 // Tìm vị trí
-function checkCart(id){
+function checkCart(id) {
   let index = -1;
-  for (let i = 0; i < cart.length; i++){
-    if(cart[i].product.id === id){
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].product.id === id) {
       index = i;
       break;
     }
@@ -128,83 +108,84 @@ function checkCart(id){
   return index;
 }
 //Cập nhật số lượng sản phẩm
-function updateQuantity(index){
-  for(let i = 0; i < cart.length; i++){
-    if(i == index){
+function updateQuantity(index) {
+  for (let i = 0; i < cart.length; i++) {
+    if (i == index) {
       cart[i].product.sl += 1;
       break;
     }
   }
 }
 
-function renderCart(cart){
+function renderCart(cart) {
+  var quantity = 0;
+  for (var i = 0; i < cart.length; i++) {
+    quantity += cart[i].quantity;
+  }
+  domId("total-qty").innerHTML = quantity;
+
   var content = "";
-  // var gh = JSON.parse(sessionStorage.getItem("cart"));
-  for(var i = 0; i < cart.length; i++){
+ 
+
+  for (var i = 0; i < cart.length; i++) {
     var total = cart[i].product.price * cart[i].product.sl;
-      content += `
-      <tr style="color: white">
-              <th style="padding: 40px 40px 0;">STT</th>
-              <th style="padding: 40px 40px 0 0;">Image</th>            
-              <th style="padding: 40px 40px 0 0;">name</th>
-              <th style="padding: 40px 40px 0 0;">Số lượng</th>
-              <th style="padding: 40px 40px 0 0;">price</th>
-              <th style="padding: 40px 40px 0 0;">Thành tiền</th>
-            </tr>
+    content +=
+      ` 
       <tr style="color: white">     
       <td style="padding-left: 50px">${cart[i].product.id}</td>
       <td><img id="image" class="product-img" src=${cart[i].product.img} alt="" style="width: 40px; height: 40px"></td>
       <td>${cart[i].product.name}</td>
       <td>
       <div class="buttons_added">
-      <input type="hidden" value= "`+i+`">
+      <input type="hidden" value= "` +
+      i +
+      `">
   <input onclick="giamsl(this)" class="minus is-form" type="button" value="-">
   <input aria-label="quantity" id="sl" class="input-qty" max="10" min="1" name="" type="number" style="width: 40px" value="${cart[i].product.sl}">
   <input onclick="tangsl(this)" class="plus is-form" type="button" value="+">
 </div>
       </td>
       <td>${cart[i].product.price}</td>
-      <td>`+total+`</td>  
+      <td>` +
+      total +
+      `</td>  
       <td><button onclick = "deletesp('${cart[i].product.id}')" class="btn btn-danger">Xóa</button></td>
       </tr>
-      `  
+      `;
   }
   domId("myCart").innerHTML = content;
-  console.log(total);
 }
 
-function showCart(){
+function showCart() {
   domId("showCart").style.display = "block";
 }
 
-function noneCart(){
+function noneCart() {
   domId("showCart").style.display = "none";
 }
 
-function tangsl(x){
+function tangsl(x) {
   let vt = x.parentElement.children[0].value;
   let sl = x.parentElement.children[2];
- 
+
   let slmoi = 0;
-  for(let i = 0; i < cart.length; i++){
-    if(i == vt){
+  for (let i = 0; i < cart.length; i++) {
+    if (i == vt) {
       slmoi = cart[i].product.sl + 1;
       cart[i].product.sl += 1;
       break;
     }
   }
-
-
   sl.value = slmoi;
   renderCart(cart);
 }
-function giamsl(x){
+function giamsl(x) {
   let vt = x.parentElement.children[0].value;
   let sl = x.parentElement.children[2];
- 
+
   let slmoi = 0;
-  for(let i = 0; i < cart.length; i++){
-    if(i == vt){
+  for (let i = 0; i < cart.length; i++) {
+    if (i == vt) {
       slmoi = cart[i].product.sl - 1;
       cart[i].product.sl -= 1;
       break;
@@ -214,70 +195,39 @@ function giamsl(x){
   renderCart(cart);
 }
 // Xóa sản phẩm ra khỏi giỏ hàng
-function deletesp(sp){
+function deletesp(sp) {
   var index = checkCart(sp);
-  if(index === -1){
+  if (index === -1) {
     alert("Không tìm thấy sản phẩm phù hợp");
     return;
   }
   cart.splice(index, 1);
   renderCart(cart);
+  saveData();
 }
 // Clear giỏ hàng về mảng rỗng
-function clearCart(){
+function clearCart() {
   var nCart = cart;
-  nCart = []; 
+  nCart = [];
   domId("myCart").innerHTML = nCart;
   domId("total-qty").innerHTML = 0;
 }
 
-
 // Lưu dữ liệu vào localStorage
-function saveData(){
+function saveData() {
   var cartListJSON = JSON.stringify(cart);
   localStorage.setItem("cart", cartListJSON);
 }
 // Đưa dữ liệu cũ trong localStorage lên màn hình
-function getData(){
-    var cartListJSON = localStorage.getItem("cart");
+function getData() {
+  cart = JSON.parse(localStorage.getItem("cart"));
 
-    // Nếu localStorage null (ko có ds) thì ko làm gì cả.
-    if(!cartListJSON) return;
-
-    // Chuyển lại chuỗi thành mảng đối tượng
-    var cartListLocal = JSON.parse(cartListJSON);
-    cart = mapData(cartListLocal);
-
-    // in lại danh sách ra màn hình
-    renderCart(cart); 
+  // Nếu localStorage null (ko có ds) thì ko làm gì cả.
+  if (!cart)return;
+  renderCart(cart);
 }
 
-// input: mảng sinh viên từ local => ouput: mảng sinh viên mới
-function mapData(dataFromLocal) {
-  var result = [];
-  for (var i = 0; i < dataFromLocal.length; i++) {
-    var oldCart = dataFromLocal[i];
-    var newCart = new cartItem(
-
-      oldCart.product.id,
-      oldCart.product.img,
-      oldCart.product.price,
-      oldCart.roduct.name,
-      oldCart.product.sl,
-      oldCart.product.money,
-
-   
-    );
-    result.push(newCart);
-  }
-
-  return result;
-}
-
-
-window.onload = function(){
-    getProductList();
-}
-
-
-
+window.onload = function () {
+  getProductList();
+  getData();
+};
--- QLSP/models/product.js
+++ QLSP/models/product.js
@@ -1,14 +1,3 @@
-function Product(name, price, screen, backCamera, frontCamera, img, desc, type){
-    this.name = name;
-    this.price = price;
-    this.screen = screen;
-    this.backCamera = backCamera;
-    this.frontCamera = frontCamera;
-    this.img = img;
-    this.desc = desc;
-    this.type = type;
-}
-
 function ProductPhan2(name, price, image, description){
     this.name = name;
     this.price = price;
